f729a4cd7276ebdaf94346d1c0b50e66e2c88775
amelia v <amelia@ag2v.com>
2025-06-19 11:10:45 -0400

    finish base functions for the library

--- src/rp2x_cat24m01.c
+++ src/rp2x_cat24m01.c
@@ -1,13 +1,28 @@
 #include "rp2x_cat24m01.h"
 
+//this is the only function that returns true if it fails - for obvious reasons
+//datasheet says to perform a selective read because the i2c simply wont ACK if it is busy with an internal write
+//instead we skip the read part and just try to write the address pointer because the rp2040 i2c will
+//still return an error if it cant communicate
+bool cat24_is_busy(int i2c_inst, uint8_t sel) {
+	uint8_t cat24_addr = 0x50 | (sel << 1);
 
-// r/w one byte at a time
+	uint8_t buffer[2] = {0x00};
+	
+	int status = i2c_write_blocking(i2c_get_instance(i2c_inst), cat24_addr, buffer, 2, false);
+	if(status != 2) return true;
+
+	return false;
+}
+
+
+// write one byte at a time
 bool cat24_write_byte(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t src) {
-	uint8_t cat24_addr = 0xA0 | (sel << 2);
-	if(page > 255) cat24_addr |= 0x02;
+	uint8_t cat24_addr = 0x50 | (sel << 1);
+	if(page > 255) cat24_addr |= 0x01;
 	uint8_t _page = page & 0x00FF;
 
-	uint8_t buffer[3] = {_page, addr, src};
+	uint8_t buffer[3] = {0x01, addr, src};
 
 	int status = i2c_write_blocking(i2c_get_instance(i2c_inst), cat24_addr, buffer, 3, false);
 	if(status != 3) return false;
@@ -16,14 +31,14 @@ bool cat24_write_byte(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t
 }
 
 
-
+// read one byte at a time
 bool cat24_read_byte(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t *dst) {
-	uint8_t cat24_addr = 0xA0 | (sel << 2);
-	if(page > 255) cat24_addr |= 0x02;
+	uint8_t cat24_addr = 0x50 | (sel << 1);
+	if(page > 255) cat24_addr |= 0x01;
 	uint8_t _page = page & 0x00FF;
 
-	uint8_t wbuffer[2] = {_page, addr};
-	int status = i2c_write_blocking(i2c_get_instance(i2c_inst), cat24_addr, wbuffer, 2, false);
+	uint8_t buffer[2] = {_page, addr};
+	int status = i2c_write_blocking(i2c_get_instance(i2c_inst), cat24_addr, buffer, 2, true);
 	if(status != 2) return false;
 
 	status = i2c_read_blocking(i2c_get_instance(i2c_inst), cat24_addr, dst, 1, false);
@@ -34,6 +49,43 @@ bool cat24_read_byte(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t 
 }
 
 
-// r/w up to a full page - 256 bytes
-bool cat24_write_page(uint8_t sel, int page, uint8_t addr, uint8_t *src, size_t length);
-bool cat24_read_page(uint8_t sel, int page, uint8_t addr, uint8_t *dst, size_t length);
+// write up to a full page - 256 bytes
+bool cat24_write_page(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t *src, size_t length) {
+	uint8_t cat24_addr = 0x50 | (sel << 1);
+	if(page > 255) cat24_addr |= 0x01;
+	uint8_t _page = page & 0x00FF;
+
+	//this gets weird; we have to slide the provided array over a little bit
+	uint8_t buffer[2 + length];
+	for(int i = 0; i < length; i++) {
+		buffer[i+2] = src[i];
+	}
+	buffer[0] = _page;
+	buffer[1] = addr;
+
+	int l = sizeof(buffer);
+
+	int status = i2c_write_blocking(i2c_get_instance(i2c_inst), cat24_addr, buffer, l, false);
+	if(status != l) return false;
+
+	return true;
+
+}
+
+
+// read up tp a full page - 256 bytes
+bool cat24_read_page(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t *dst, size_t length) {
+	uint8_t cat24_addr = 0x50 | (sel << 1);
+	if(page > 255) cat24_addr |= 0x01;
+	uint8_t _page = page & 0x00FF;
+
+	uint8_t buffer[2] = {_page, addr};
+	int status = i2c_write_blocking(i2c_get_instance(i2c_inst), cat24_addr, buffer, 2, true);
+	if(status != 2) return false;
+
+	status = i2c_read_blocking(i2c_get_instance(i2c_inst), cat24_addr, dst, length, false);
+	if(status != length) return false;
+
+	return true;
+
+}
--- src/rp2x_cat24m01.h
+++ src/rp2x_cat24m01.h
@@ -1,12 +1,71 @@
+//      Copyright (C) 2025
+//      Evan Morse
+//      Amelia Vlahogiannis
+//      Sam Cowan
+//      Marin Bove
+
+//      This program is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation, either version 3 of the License, or
+//      (at your option) any later version.
+
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+
+//      You should have received a copy of the GNU General Public License
+//      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 #include "hardware/i2c.h"
 #include <stdbool.h>
 
+
+/*
+ sample procedure to init:
+ i2c_init(I2C_INST, 100 * 1000);
+ gpio_set_function(PIN_SCL, GPIO_FUNC_I2C);
+ gpio_set_function(PIN_SDA, GPIO_FUNC_I2C);
+ gpio_pull_up(PIN_SCL);
+ gpio_pull_up(PIN_SDA);
+ uint index = I2C_NUM(I2C_INST);
+*/
+
+/*
+ * using this library:
+ * the device is very simple and can perform either read or write operations and not much else
+ * read/write_byte functions take a single bit at a time- 
+ * 	usage:
+ * 	cat24_write_byte(index, 0x00, 420, 0x45, write_buffer);
+ * 	where:
+ * 		index is the i2c number (see init procedure above)
+ * 		sel (0x00) is the device address set by resistors on A2 and A1
+ * 		420 is the page number (0-511)
+ * 		0x45 is the address within that page (0-255)
+ * 		write_buffer is a uint8_t to write to that address
+ *
+ * 	or:
+ * 	cat24_write_page(index, 0x00, 0x01, 0x00, write_string, sizeof(write_string));
+ * 	where:
+ * 		write_string is a uint8_t array up to 255 elements
+ * 	
+ *
+ * read/write functions return a 1 if i2c communication is successful and a 0 if not.
+ * cat24_is_busy returns the opposite, because that made more sense to me when i wrote this
+ */
+
+
+
+
+// check if an internal write operation is happening
+bool cat24_is_busy(int i2c_inst, uint8_t sel);
+
 // r/w one byte at a time
 bool cat24_write_byte(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t src);
-
 bool cat24_read_byte(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t *dst);
 
 
 // r/w up to a full page - 256 bytes
-bool cat24_write_page(uint8_t sel, int page, uint8_t addr, uint8_t *src, size_t length);
-bool cat24_read_page(uint8_t sel, int page, uint8_t addr, uint8_t *dst, size_t length);
+bool cat24_write_page(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t *src, size_t length);
+bool cat24_read_page(int i2c_inst, uint8_t sel, int page, uint8_t addr, uint8_t *dst, size_t length);
